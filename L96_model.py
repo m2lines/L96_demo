469ec69caa5491b27189f1605fe1bbc6ab818c60
Alistair Adcroft <adcroft@users.noreply.github.com>
2021-02-24 10:50:01 -0500

    Re-factor Lorenz_96()
    - Removes extra function argument, "J", which is implied by size of "X" and thus redundant.
    - Also simplifed implementation of periodicity by add replacing `j+1` with `(j_1)%J`. This allows the loop to work over the full range and removes the need for the special handling of j=0,1,J-1.

--- L96_model.py
+++ L96_model.py
@@ -6,24 +6,21 @@ https://www.ecmwf.int/en/elibrary/10829-predictability-problem-partly-solved
 
 import numpy as np 
 
-def Lorenz_96(X,t,F,J):
+def Lorenz_96(X,t,F):
     """
-    Calculate the time increment in the X variables for the Lorenz '96.
+    Calculate the time rate of change for the X variables for the Lorenz '96.
     Args:
         X : Values of X variables at the current time step
         F : Forcing term
-        J : Number of variables
         t : Time
     Returns:
         dXdt : Array of X time tendencies
     """
-  
-    s = np.zeros(J);
-    s[0] = (X[1]-X[J-2])*X[J-1]-X[0];
-    s[1] = (X[2]-X[J-1])*X[0]-X[1];
-    s[J-1] = (X[0]-X[J-3])*X[J-2]-X[J-1];
+
+    J = len(X)
+    s = np.zeros(J)
     
-    for j in range(2,J-1):
-        s[j] = (X[j+1]-X[j-2])*X[j-1]-X[j];
-    dXdt = s.T + F;
+    for j in range(J):
+        s[j] = (X[(j+1)%J]-X[j-2])*X[j-1]-X[j]
+    dXdt = s.T + F
     return dXdt
